b41874600a8858e43b130cf1c1824fe8a6343e79
atemengue <moafembe@gmail.com>
2019-09-04 08:40:25 +0100

    @output raising an Event

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
 
 @NgModule({
-  declarations: [EventsAppComponent, EventsListComponent],
+  declarations: [EventsAppComponent, EventsListComponent, EventThumbnailComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [EventsAppComponent]

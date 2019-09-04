c501b3eb9b227c904933c360f163e96390ea18d4
atemengue <moafembe@gmail.com>
2019-09-04 10:54:25 +0100

    Creating Web Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { EventsListComponent } from './events/events-list.component';
+import { EventService } from './events/shared/event.service';
 import { NavbarComponent } from './nav/navbar.component';
 
 @NgModule({
-  declarations: [EventsAppComponent, EventsListComponent, EventThumbnailComponent, NavbarComponent],
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavbarComponent
+  ],
   imports: [BrowserModule],
-  providers: [],
+  providers: [EventService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule {}

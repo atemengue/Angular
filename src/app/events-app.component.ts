0ee9ab2fc85a80e7d02dfe58f057e848668ba33f
atemengue <moafembe@gmail.com>
2019-09-04 08:50:52 +0100

    using template variable for communication between component

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app-root',
+  template: `
+    <events-list></events-list>
+  `
+})
+export class EventsAppComponent {
+  title = 'ps-demo';
+}
---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'event-thumbnail',
+  templateUrl: './event-thumbnail.component.html',
+  styleUrls: ['./event-thumbnail.component.css']
+})
+export class EventThumbnailComponent implements OnInit {
+  constructor() {}
+
+  @Input() event: any;
+  ngOnInit() {}
+
+  logFoo() {
+    console.log('this is regis');
+  }
+}

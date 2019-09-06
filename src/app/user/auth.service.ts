abc8960c576abb259559f89c59230b842f89086d
atemengue <moafembe@gmail.com>
2019-09-06 06:14:21 +0100

    add auth Service

---
+++ src/app/user/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { IUser } from './user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  currentUser: IUser;
+  constructor() {}
+
+  loginUser(userName: string, password: string) {
+    this.currentUser = {
+      id: 1,
+      userName: userName,
+      firstName: 'John',
+      lastName: 'Papa'
+    };
+  }
+
+  isAuthenticated() {
+    return !!this.currentUser;
+  }
+}

90fd663f83115823c07095f71f0950041543880f
atemengue <moafembe@gmail.com>
2019-09-06 06:33:45 +0100

    ok template form

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -10,6 +10,7 @@ import { AuthService } from './auth.service';
 export class LoginComponent implements OnInit {
   username: string;
   password: string;
+  mouseoverLogin: boolean;
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {}
